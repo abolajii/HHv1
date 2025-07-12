29713df403c81bfa74e7195817a5135b4c74fc32
abolajii <abolajiadeajayi@gmail.com>
2025-07-12 20:21:50 +0100

    hh up

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-      "https://amsfev1.vercel.app",
-    ],
+    origin: "*",
   })
 );
 
